0fc928337187489bdbf428159061a98eae130812
Ruhul Amin <brainboostservices@gmail.com>
2025-07-09 01:16:16 +0600

    A Fake Push

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-gray-50 dfd">
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
