3b8d8bf964898584a95e72454efb84b280e8bd77
Ruhul Amin <brainboostservices@gmail.com>
2025-07-09 01:03:42 +0600

    Add eslint-disable for no-explicit-any in multiple components

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 import { Search, Settings, User } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
     DropdownMenu,
     DropdownMenuContent,
